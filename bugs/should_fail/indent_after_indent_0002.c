abee4fda78417f4667b1cd41a30cc95296a05e98
roblabla <robinlambertz+dev@gmail.com>
2015-12-02 14:41:43 +0100

    And this one should fail.

---
+++ bugs/should_fail/indent_after_indent_0002.c
@@ -0,0 +1,13 @@
+/* rules: CheckIndentationInsideFunction */
+void	main(void)
+{
+	if (ptr == (void *)block + ((i) * M_MINUS) + 520)
+	{
+		if (size <= M_MINUS)
+			block->tab[i] = size;
+		else
+			return (ft_memcpy(large_size(size),
+					(void *)block + (i * M_MINUS) + 520, size));
+				return ((void *)block + (i * M_MINUS) + 520);
+	}
+}
